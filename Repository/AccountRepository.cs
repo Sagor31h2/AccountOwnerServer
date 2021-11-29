313a58faba415a8120ebbed0714e3d54f53671d1
Sagor31h2 <sagor31h2@gmail.com>
2021-11-29 20:41:03 +0600

    Add repository pattern

---
+++ Repository/AccountRepository.cs
@@ -0,0 +1,15 @@
+﻿using Contracts;
+using Entities;
+using Entities.Models;
+
+namespace Repository
+{
+    public class AccountRepository : RepositoryBase<Account>, IAccountRepository
+    {
+        public AccountRepository(RepoContext repositoryContext)
+            : base(repositoryContext)
+        {
+
+        }
+    }
+}
